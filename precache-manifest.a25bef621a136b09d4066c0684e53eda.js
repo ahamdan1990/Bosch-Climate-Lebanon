c08ffee4afe90934363b0407f08e8960cd9267c3
Ali S. Hamdan <ahamdan@SecurityEng.com>
2022-10-28 14:10:32 +0300

    initial commit

---
+++ precache-manifest.a25bef621a136b09d4066c0684e53eda.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cb6d8900e2464c24ebe",
+    "url": "/static/css/main.9fb4b5ff.chunk.css"
+  },
+  {
+    "revision": "0cb6d8900e2464c24ebe",
+    "url": "/static/js/main.0cb6d890.chunk.js"
+  },
+  {
+    "revision": "8c8777f317c62c0b5290",
+    "url": "/static/js/1.8c8777f3.chunk.js"
+  },
+  {
+    "revision": "0aa030a704c0f0f105e7",
+    "url": "/static/css/2.9ee8dd34.chunk.css"
+  },
+  {
+    "revision": "0aa030a704c0f0f105e7",
+    "url": "/static/js/2.0aa030a7.chunk.js"
+  },
+  {
+    "revision": "469ce6c24766fefd860f",
+    "url": "/static/js/runtime~main.469ce6c2.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "5db0d92638a70e352af2819929b7e274",
+    "url": "/index.html"
+  }
+];
